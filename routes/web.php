2a8d6e86cb6ccb89270999cbd0141933ee4d5b85
To Van Tiep <tovantiep2604@gmail.com>
2021-12-06 17:08:29 +0700

    template admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+});
+Route::prefix('admin')->group(function(){
+    Route::get('home',[HomeController::class, 'home'])->name('admin.hone.home');
 });

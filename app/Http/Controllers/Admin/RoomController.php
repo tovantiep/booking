14d519469947232bdc686e32f6d89bb96791e0cd
To Van Tiep <tovantiep2604@gmail.com>
2021-12-28 15:37:58 +0700

    update and delete function room

--- app/Http/Controllers/Admin/RoomController.php
+++ app/Http/Controllers/Admin/RoomController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Room\CreateRoomRequest;
+use App\Http\Requests\Room\UpdateRoomRequest;
 use App\Models\Category;
 use App\Models\Room;
 use Illuminate\Http\Request;
@@ -47,5 +48,46 @@ class RoomController extends Controller
       $room->description = $request->description;
       $room->save();
       return redirect()->route('admin.room.index')->with('success', 'Thêm thành công ');
+    }
+    public function edit($id)
+    {
+        $room = Room::find($id);
+        $categories = Category::all();
+        return view('admin.room.edit', compact('room', 'categories'));
+    }
+    public function update(UpdateRoomRequest $request, $id)
+    {
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $name_file = $file->getClientOriginalName();
+            $extention = $file->getClientOriginalExtension();
+            if(strcasecmp($extention, 'jpg') == 0
+               || strcasecmp($extention, 'png') == 0
+               || strcasecmp($extention, 'jepg') == 0) {
+                   $image = Str::random(5)."_".$name_file;
+                   while(file_exists("image/post".$image)){
+                       $image = Str::random(5)."_".$name_file;
+                   }
+                   $file->move('image/post', $image);
+               }
+         }
+         $room = Room::find($id);
+         $room->update([
+           'category_id' => $request->category_id,
+           'branch' => $request->branch,
+            'people' => $request->people,
+           'floor' => $request->floor,
+            'number_room' => $request->number_room,
+            'total_money' => $request->total_money,
+            'image' => isset($image) ? $image : $room->image,
+            'description' => $request->description,
+         ]);
+         return redirect()->route('admin.room.index', $id)->with('success', 'Update thành công ');
+    }
+    public function delete($id)
+    {
+        $room = Room::find($id);
+        $room->delete();
+        return redirect()->route('admin.room.index', $id)->with('success', 'Xóa thành công');
     }
 }
